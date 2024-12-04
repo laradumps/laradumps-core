479bba7261af124c316d21af4986221fdd128c1d
Dan <daniel@sysanalyst.eu>
2024-12-04 14:01:58 +0100

    add return type on ds helper

--- src/functions.php
+++ src/functions.php
@@ -21,7 +21,7 @@ if (!function_exists('appBasePath')) {
 }
 
 if (!function_exists('ds')) {
-    function ds(mixed ...$args)
+    function ds(mixed ...$args): LaraDumps|LaravelLaraDumps
     {
         $sendRequest = function ($args, LaraDumps $instance) {
             if ($args) {
