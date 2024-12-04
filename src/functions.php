9250c30a97bdb0e1405bb822ea495efd5a8fa24b
Dan <daniel@sysanalyst.eu>
2024-12-04 13:27:30 +0100

    f

--- src/functions.php
+++ src/functions.php
@@ -21,7 +21,7 @@ if (!function_exists('appBasePath')) {
 }
 
 if (!function_exists('ds')) {
-    function ds(mixed ...$args): LaraDumps|\LaraDumps\LaraDumps\LaraDumps
+    function ds(mixed ...$args): LaraDumps|LaravelLaraDumps
     {
         $sendRequest = function ($args, LaraDumps $instance) {
             if ($args) {
