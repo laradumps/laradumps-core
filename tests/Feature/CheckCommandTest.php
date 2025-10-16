39592e1de91e9aa106aea23e2e75bd81eabef00d
Leo Lutz <leo@leolutz.com>
2025-10-16 15:02:14 -0600

    Add failing test, fix test title's grammar, and fix ansi issues.
    For some reason adding --no-ansi was not disabling ansi colors for the code sections, so I added a function to strip them.

--- tests/Feature/CheckCommandTest.php
+++ tests/Feature/CheckCommandTest.php
@@ -21,28 +21,33 @@ function startCommandApplication(array $arguments): CommandTester
     return $commandTester;
 }
 
-it('show message if variable ... is empty', function () {
+function stripAnsiColors(string $text): string
+{
+    return preg_replace('/\e\[[0-9;]*[mK]/', '', $text);
+}
+
+it('shows message if "dir" parameter is empty', function () {
     $commandTester = startCommandApplication([]);
 
-    $output = $commandTester->getDisplay();
+    $output = stripAnsiColors($commandTester->getDisplay());
 
     expect($output)
         ->toContain('Whoops. Specify the folders you need to search in --dir option in the comma separated');
 });
 
-it('check command work property', function () {
+it('checks command works properly', function () {
     $commandTester = startCommandApplication([
         '--dir'          => sprintf('tests%sFixtures', DIRECTORY_SEPARATOR),
         '--ignore-files' => vsprintf('tests%sFixtures%sds_env, tests%sFixtures%sAnotherFunctionsToCheck.php', [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR]),
     ]);
 
-    $output = $commandTester->getDisplay();
+    $output = stripAnsiColors($commandTester->getDisplay());
 
     expect($output)
         ->toContain('LaraDumps is searching for words used in debugging in: ' . sprintf('tests%sFixtures', DIRECTORY_SEPARATOR))
         ->and($output)
         ->not->toContain('Whoops. Specify the folders you need to search in --dir option in the comma separated')
-        ->toContain('1/2')
+        ->toContain('1/3')
         ->toContain(
             'ds(\'this is a function to check!\')',
             '@ds("this is a function to check!")',
@@ -52,47 +57,51 @@ it('check command work property', function () {
             'dd(\'this is a function to check!\')',
             '//ds(\'this is a function to check!\')'
         )
-        ->toContain('ERROR - Found 2 errors / 1 file');
+        ->toContain('ERROR - Found 4 errors / 2 files');
 });
 
-it('check command with "dump", "dd" work property', function () {
+it('checks command with "dump", "dd" works properly', function () {
     $commandTester = startCommandApplication([
         '--dir'          => sprintf('tests%sFixtures', DIRECTORY_SEPARATOR),
         '--ignore-files' => vsprintf('tests%sFixtures%sds_env, tests%sFixtures%sExampleClassToCheck.php', [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR]),
         '--text'         => 'dump,dd',
     ]);
 
-    $output = $commandTester->getDisplay();
+    $output = stripAnsiColors($commandTester->getDisplay());
 
     expect($output)
         ->toContain('LaraDumps is searching for words used in debugging in: ' . sprintf('tests%sFixtures', DIRECTORY_SEPARATOR))
         ->and($output)
         ->not->toContain('Whoops. Specify the folders you need to search in --dir option in the comma separated')
-        ->toContain('1/2')
+        ->toContain('1/3')
         ->toContain(
             'dump(\'this is a function to check!\')',
             'dd(\'this is a function to check!\')',
-            '//dd(\'this is a function to check!\')'
+            '//dd(\'this is a function to check!\')',
+            'dd(\'this is a blade dd function to check!\')',
+            '@dd(\'this is a blade dd directive to check!\')',
         )
-        ->toContain('ERROR - Found 3 errors / 1 file');
+        ->toContain('ERROR - Found 7 errors / 2 files');
 });
 
-it('check command without "dump", "dd" work property', function () {
+it('checks command without "dump", "dd" works property', function () {
     $commandTester = startCommandApplication([
         '--dir'          => sprintf('tests%sFixtures', DIRECTORY_SEPARATOR),
         '--ignore-files' => vsprintf('tests%sFixtures%sds_env, tests%sFixtures%sAnotherFunctionsToCheck.php', [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR]),
     ]);
 
-    $output = $commandTester->getDisplay();
+    $output = stripAnsiColors($commandTester->getDisplay());
 
     expect($output)
         ->toContain('LaraDumps is searching for words used in debugging in: ' . sprintf('tests%sFixtures', DIRECTORY_SEPARATOR))
         ->and($output)
         ->not->toContain('Whoops. Specify the folders you need to search in --dir option in the comma separated')
-        ->toContain('1/2')
+        ->toContain('1/3')
         ->toContain(
             'ds(\'this is a function to check!\');',
             ' @ds("this is a function to check!")',
+            'ds(\'this is a blade function to check!\')',
+            '@ds(\'this is a blade directive to check!\')',
         )
-        ->toContain('ERROR - Found 2 errors / 1 file');
+        ->toContain('ERROR - Found 4 errors / 2 files');
 });
---
+++ tests/Fixtures/template-to-check.blade.php
@@ -0,0 +1,7 @@
+<?php
+    ds('this is a blade function to check!');
+    dd('this is a blade dd function to check!');
+?>
+
+@ds('this is a blade directive to check!')
+@dd('this is a blade dd directive to check!')
